a8e2db0a5c7da22c9bdda1e40fd614af8a73a0cb
DAMAS ANDREAN <andreandamas@gmail.com>
2019-09-09 11:46:05 +0700

    TebakAngka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -51,10 +51,10 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	public void handleReset(View view) {
-		// TODO: Reset 
+		// TODO: Reset tampilan
 		number_input.setText("");
 		initRandomNumber();
 		Toast.makeText(this, ""+random,Toast.LENGTH_SHORT).show();
-
+		
 	}
 }
